c8c782a536fe6e4460c8dcb653e9501e90fc677d
Hardy Ferentschik <hardy@hibernate.org>
2011-08-14 23:57:49 +0200

    HHH-6537 Moving createIdentifierGenerator from BasicAttributeBinding to IdentifierGeneratorResolver

--- hibernate-core/src/main/java/org/hibernate/metamodel/binding/BasicAttributeBinding.java
+++ hibernate-core/src/main/java/org/hibernate/metamodel/binding/BasicAttributeBinding.java
@@ -23,17 +23,8 @@
  */
 package org.hibernate.metamodel.binding;
 
-import java.util.Properties;
-
-import org.hibernate.MappingException;
-import org.hibernate.id.IdentifierGenerator;
-import org.hibernate.id.PersistentIdentifierGenerator;
-import org.hibernate.id.factory.IdentifierGeneratorFactory;
 import org.hibernate.mapping.PropertyGeneration;
 import org.hibernate.metamodel.domain.SingularAttribute;
-import org.hibernate.metamodel.relational.Column;
-import org.hibernate.metamodel.relational.Schema;
-import org.hibernate.metamodel.relational.SimpleValue;
 import org.hibernate.metamodel.source.MetaAttributeContext;
 
 /**
@@ -43,7 +34,7 @@ import org.hibernate.metamodel.source.MetaAttributeContext;
  */
 public class BasicAttributeBinding
 		extends AbstractSingularAttributeBinding
-		implements  KeyValueBinding {
+		implements KeyValueBinding {
 
 	private String unsavedValue;
 	private PropertyGeneration generation;
@@ -119,79 +110,5 @@ public class BasicAttributeBinding
 
 	public void setMetaAttributeContext(MetaAttributeContext metaAttributeContext) {
 		this.metaAttributeContext = metaAttributeContext;
-	}
-
-	public IdentifierGenerator createIdentifierGenerator(
-			IdGenerator idGenerator,
-			IdentifierGeneratorFactory identifierGeneratorFactory,
-			Properties properties) {
-		Properties params = new Properties();
-		params.putAll( properties );
-
-		// use the schema/catalog specified by getValue().getTable() - but note that
-		// if the schema/catalog were specified as params, they will already be initialized and
-		//will override the values set here (they are in idGenerator.getParameters().)
-		Schema schema = getValue().getTable().getSchema();
-		if ( schema != null ) {
-			if ( schema.getName().getSchema() != null ) {
-				params.setProperty( PersistentIdentifierGenerator.SCHEMA, schema.getName().getSchema().getName() );
-			}
-			if ( schema.getName().getCatalog() != null ) {
-				params.setProperty( PersistentIdentifierGenerator.CATALOG, schema.getName().getCatalog().getName() );
-			}
-		}
-
-		// TODO: not sure how this works for collection IDs...
-		//pass the entity-name, if not a collection-id
-		//if ( rootClass!=null) {
-			params.setProperty( IdentifierGenerator.ENTITY_NAME, getContainer().seekEntityBinding().getEntity().getName() );
-		//}
-
-		//init the table here instead of earlier, so that we can get a quoted table name
-		//TODO: would it be better to simply pass the qualified table name, instead of
-		//      splitting it up into schema/catalog/table names
-		String tableName = getValue().getTable().getQualifiedName( identifierGeneratorFactory.getDialect() );
-		params.setProperty( PersistentIdentifierGenerator.TABLE, tableName );
-
-		//pass the column name (a generated id almost always has a single column)
-		if ( getSimpleValueSpan() > 1 ) {
-			throw new MappingException(
-					"A SimpleAttributeBinding used for an identifier has more than 1 Value: " + getAttribute().getName()
-			);
-		}
-		SimpleValue simpleValue = (SimpleValue) getValue();
-		if ( !Column.class.isInstance( simpleValue ) ) {
-			throw new MappingException(
-					"Cannot create an IdentifierGenerator because the value is not a column: " +
-							simpleValue.toLoggableString()
-			);
-		}
-		params.setProperty(
-				PersistentIdentifierGenerator.PK,
-				( (Column) simpleValue ).getColumnName().encloseInQuotesIfQuoted(
-						identifierGeneratorFactory.getDialect()
-				)
-		);
-
-		// TODO: is this stuff necessary for SimpleValue???
-		//if (rootClass!=null) {
-		//	StringBuffer tables = new StringBuffer();
-		//	Iterator iter = rootClass.getIdentityTables().iterator();
-		//	while ( iter.hasNext() ) {
-		//		Table table= (Table) iter.next();
-		//		tables.append( table.getQuotedName(dialect) );
-		//		if ( iter.hasNext() ) tables.append(", ");
-		//	}
-		//	params.setProperty( PersistentIdentifierGenerator.TABLES, tables.toString() );
-		//}
-		//else {
-		params.setProperty( PersistentIdentifierGenerator.TABLES, tableName );
-		//}
-
-		params.putAll( idGenerator.getParameters() );
-
-		return identifierGeneratorFactory.createIdentifierGenerator(
-				idGenerator.getStrategy(), getHibernateTypeDescriptor().getResolvedTypeMapping(), params
-		);
 	}
 }
--- hibernate-core/src/main/java/org/hibernate/metamodel/source/internal/IdentifierGeneratorResolver.java
+++ hibernate-core/src/main/java/org/hibernate/metamodel/source/internal/IdentifierGeneratorResolver.java
@@ -24,18 +24,23 @@
 package org.hibernate.metamodel.source.internal;
 
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.hibernate.MappingException;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.NamingStrategy;
 import org.hibernate.cfg.ObjectNameNormalizer;
 import org.hibernate.id.IdentifierGenerator;
 import org.hibernate.id.PersistentIdentifierGenerator;
+import org.hibernate.id.factory.IdentifierGeneratorFactory;
 import org.hibernate.metamodel.binding.BasicAttributeBinding;
 import org.hibernate.metamodel.binding.EntityBinding;
 import org.hibernate.metamodel.binding.EntityIdentifier;
+import org.hibernate.metamodel.binding.IdGenerator;
+import org.hibernate.metamodel.relational.Column;
+import org.hibernate.metamodel.relational.Schema;
+import org.hibernate.metamodel.relational.SimpleValue;
 import org.hibernate.metamodel.source.MetadataImplementor;
 import org.hibernate.service.config.spi.ConfigurationService;
 
@@ -50,41 +55,126 @@ public class IdentifierGeneratorResolver {
 		this.metadata = metadata;
 	}
 
-	// IdentifierGeneratorResolver.resolve() must execute after AttributeTypeResolver.resolve()
-	// to ensure that identifier type is resolved.
 	@SuppressWarnings( { "unchecked" })
 	void resolve(Map<String, IdentifierGenerator> identifierGenerators) {
 		for ( EntityBinding entityBinding : metadata.getEntityBindings() ) {
-			if ( entityBinding.isRoot() ) {
-				Properties properties = new Properties();
-				properties.putAll(
-						metadata.getServiceRegistry()
-								.getService( ConfigurationService.class )
-								.getSettings()
-				);
-				//TODO: where should these be added???
-				if ( !properties.contains( AvailableSettings.PREFER_POOLED_VALUES_LO ) ) {
-					properties.put( AvailableSettings.PREFER_POOLED_VALUES_LO, "false" );
-				}
-				if ( !properties.contains( PersistentIdentifierGenerator.IDENTIFIER_NORMALIZER ) ) {
-					properties.put(
-							PersistentIdentifierGenerator.IDENTIFIER_NORMALIZER,
-							new ObjectNameNormalizerImpl( metadata )
-					);
-				}
+			if ( !entityBinding.isRoot() ) {
+				continue;
+			}
 
-				EntityIdentifier entityIdentifier = entityBinding.getHierarchyDetails().getEntityIdentifier();
-				if ( entityIdentifier.getIdGenerator() != null && entityIdentifier.isSimple() ) {
-					IdentifierGenerator identifierGenerator = ( (BasicAttributeBinding) entityIdentifier.getValueBinding() )
-							.createIdentifierGenerator(
-									entityIdentifier.getIdGenerator(),
-									metadata.getIdentifierGeneratorFactory(),
-									properties
-							);
-					identifierGenerators.put( entityBinding.getEntity().getName(), identifierGenerator );
-				}
+			Properties properties = new Properties();
+			properties.putAll(
+					metadata.getServiceRegistry()
+							.getService( ConfigurationService.class )
+							.getSettings()
+			);
+
+			//TODO: where should these be added???
+			if ( !properties.contains( AvailableSettings.PREFER_POOLED_VALUES_LO ) ) {
+				properties.put( AvailableSettings.PREFER_POOLED_VALUES_LO, "false" );
+			}
+			if ( !properties.contains( PersistentIdentifierGenerator.IDENTIFIER_NORMALIZER ) ) {
+				properties.put(
+						PersistentIdentifierGenerator.IDENTIFIER_NORMALIZER,
+						new ObjectNameNormalizerImpl( metadata )
+				);
+			}
+
+			EntityIdentifier entityIdentifier = entityBinding.getHierarchyDetails().getEntityIdentifier();
+			if ( entityIdentifier.getIdGenerator() != null && entityIdentifier.isSimple() ) {
+				BasicAttributeBinding attributeBinding = ( (BasicAttributeBinding) entityIdentifier.getValueBinding() );
+				IdentifierGenerator identifierGenerator = createIdentifierGenerator(
+						attributeBinding,
+						entityIdentifier.getIdGenerator(),
+						metadata.getIdentifierGeneratorFactory(),
+						properties
+				);
+				identifierGenerators.put( entityBinding.getEntity().getName(), identifierGenerator );
 			}
 		}
+	}
+
+	private IdentifierGenerator createIdentifierGenerator(
+			BasicAttributeBinding attributeBinding,
+			IdGenerator idGenerator,
+			IdentifierGeneratorFactory identifierGeneratorFactory,
+			Properties properties) {
+		Properties params = new Properties();
+		params.putAll( properties );
+
+		// use the schema/catalog specified by getValue().getTable() - but note that
+		// if the schema/catalog were specified as params, they will already be initialized and
+		//will override the values set here (they are in idGenerator.getParameters().)
+		Schema schema = attributeBinding.getValue().getTable().getSchema();
+		if ( schema != null ) {
+			if ( schema.getName().getSchema() != null ) {
+				params.setProperty( PersistentIdentifierGenerator.SCHEMA, schema.getName().getSchema().getName() );
+			}
+			if ( schema.getName().getCatalog() != null ) {
+				params.setProperty( PersistentIdentifierGenerator.CATALOG, schema.getName().getCatalog().getName() );
+			}
+		}
+
+		// TODO: not sure how this works for collection IDs...
+		//pass the entity-name, if not a collection-id
+		//if ( rootClass!=null) {
+		params.setProperty(
+				IdentifierGenerator.ENTITY_NAME,
+				attributeBinding.getContainer().seekEntityBinding().getEntity().getName()
+		);
+		//}
+
+		//init the table here instead of earlier, so that we can get a quoted table name
+		//TODO: would it be better to simply pass the qualified table name, instead of
+		//      splitting it up into schema/catalog/table names
+		String tableName = attributeBinding.getValue()
+				.getTable()
+				.getQualifiedName( identifierGeneratorFactory.getDialect() );
+		params.setProperty( PersistentIdentifierGenerator.TABLE, tableName );
+
+		//pass the column name (a generated id almost always has a single column)
+		if ( attributeBinding.getSimpleValueSpan() > 1 ) {
+			throw new MappingException(
+					"A SimpleAttributeBinding used for an identifier has more than 1 Value: " + attributeBinding.getAttribute()
+							.getName()
+			);
+		}
+		SimpleValue simpleValue = (SimpleValue) attributeBinding.getValue();
+		if ( !Column.class.isInstance( simpleValue ) ) {
+			throw new MappingException(
+					"Cannot create an IdentifierGenerator because the value is not a column: " +
+							simpleValue.toLoggableString()
+			);
+		}
+		params.setProperty(
+				PersistentIdentifierGenerator.PK,
+				( (Column) simpleValue ).getColumnName().encloseInQuotesIfQuoted(
+						identifierGeneratorFactory.getDialect()
+				)
+		);
+
+		// TODO: is this stuff necessary for SimpleValue???
+		//if (rootClass!=null) {
+		//	StringBuffer tables = new StringBuffer();
+		//	Iterator iter = rootClass.getIdentityTables().iterator();
+		//	while ( iter.hasNext() ) {
+		//		Table table= (Table) iter.next();
+		//		tables.append( table.getQuotedName(dialect) );
+		//		if ( iter.hasNext() ) tables.append(", ");
+		//	}
+		//	params.setProperty( PersistentIdentifierGenerator.TABLES, tables.toString() );
+		//}
+		//else {
+		params.setProperty( PersistentIdentifierGenerator.TABLES, tableName );
+		//}
+
+		params.putAll( idGenerator.getParameters() );
+
+		return identifierGeneratorFactory.createIdentifierGenerator(
+				idGenerator.getStrategy(),
+				attributeBinding.getHibernateTypeDescriptor().getResolvedTypeMapping(),
+				params
+		);
 	}
 
 	private static class ObjectNameNormalizerImpl extends ObjectNameNormalizer implements Serializable {
